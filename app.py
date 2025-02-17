4fc63e795b91fcd51669b421273f3e8221837b83
Pandu Hafizh Ananta <114291289+Skygers@users.noreply.github.com>
2025-02-17 11:05:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,15 @@ from collections import Counter
 import seaborn as sns
 import plotly.express as px
 
+nltk_data_dir = "./resources/nltk_data_dir/"
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir, exist_ok=True)
+
+nltk.data.path.clear()
+nltk.data.path.append(nltk_data_dir)
+nltk.download("stopwords", download_dir=nltk_data_dir)
+nltk.download("punkt", download_dir=nltk_data_dir)
+
 tokenizer = RegexpTokenizer(r'\w+')
 stop_words = set(stopwords.words('indonesian'))
 stemmer = StemmerFactory().create_stemmer()
